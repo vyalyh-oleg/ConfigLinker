3f0cce271d60322f89b4dae054295a39cba755c2
Oleg Vyalyh <oleg.vyalyh@2me.link>
2017-12-22 00:31:59 +0200

    Change methods in Deserializer interfaces as defaults.

--- src/main/java/com/configlinker/Deserializer.java
+++ src/main/java/com/configlinker/Deserializer.java
@@ -1,9 +1,9 @@
 package com.configlinker;
 
-
 import java.util.Map;
 
+
 public interface Deserializer<T> {
-	T deserialize(Map<String,String> rawValue);
-	T deserialize(String rawValue);
+	default T deserialize(Map<String,String> rawValue) { return null; };
+	default T deserialize(String rawValue) { return null; };
 }
