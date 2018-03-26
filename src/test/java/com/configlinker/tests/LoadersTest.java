8612e67261702c00d635d5e0f06fdb96b8d26637
Oleg Vyalyh <oleg.vyalyh@2me.link>
2018-03-26 17:34:03 +0300

    LoadersTest template and examples of properties files for tests.

---
+++ src/test/java/com/configlinker/tests/LoadersTest.java
@@ -0,0 +1,34 @@
+package com.configlinker.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+class LoadersTest
+{
+  @Test
+  void test_ClasspathLoader()
+  {
+
+  }
+
+  @Test
+  void test_PropertyFileLoader()
+  {
+
+  }
+
+  @Test
+  void test_HttpLoader()
+  {
+
+  }
+
+  @Disabled
+  @Test
+  void test_ConfigLinkerLoader()
+  {
+    Assertions.fail("Not implemented.");
+  }
+}
