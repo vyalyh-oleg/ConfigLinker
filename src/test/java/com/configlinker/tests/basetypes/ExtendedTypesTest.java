bdba75b32795d371f5167fede737a8780d8bf418
Oleg Vyalyh <oleg.vyalyh@2me.link>
2018-06-01 00:09:27 +0300

    Tests.

--- src/test/java/com/configlinker/tests/basetypes/ExtendedTypesTest.java
+++ src/test/java/com/configlinker/tests/basetypes/ExtendedTypesTest.java
@@ -1,6 +1,28 @@
 package com.configlinker.tests.basetypes;
 
 
+import com.configlinker.ConfigSet;
+import com.configlinker.ConfigSetFactory;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 public class ExtendedTypesTest
 {
+	private ConfigSet getConfigSet(Class<?>... interfaceType)
+	{
+		Set<Class<?>> configClasses = new HashSet<>(Arrays.asList(interfaceType));
+		return ConfigSetFactory.create(configClasses);
+	}
+	
+	private <T> T getSingleConfigInstance(Class<T> interfaceType)
+	{
+		return getConfigSet(interfaceType).getConfigObject(interfaceType);
+	}
+	
+	
 }
