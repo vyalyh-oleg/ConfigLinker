c4c16c6c3e7ab041ee977ab8d2c98a4e8db4b07c
Oleg Vyalyh <oleg.vyalyh@2me.link>
2019-01-22 19:04:50 +0200

    Added tests for track changes and ChangeListener.

--- src/test/java/com/configlinker/tests/PropertyVariableSubstitutionTest.java
+++ src/test/java/com/configlinker/tests/PropertyVariableSubstitutionTest.java
@@ -1,11 +1,50 @@
 package com.configlinker.tests;
 
 
+import com.configlinker.annotations.BoundObject;
+import com.configlinker.annotations.BoundProperty;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 
 @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
-public class PropertyVariableSubstitutionTest extends AbstractBaseTest
+class PropertyVariableSubstitutionTest extends AbstractBaseTest
 {
 	// propertyNamePrefix in BoundObject
+	
+	@Test
+	void test1()
+	{
+	
+	}
+}
+
+
+@BoundObject(sourcePath = "${substitution1}/path_part1/${substitution2}/path_part2/endPart")
+interface PropertyWithPrefix1
+{
+	@BoundProperty(name = "ordinary.value")
+	String value();
+}
+
+@BoundObject(sourcePath = "${substitution1}/path_part1/${substitution2}/path_part2/endPart")
+interface PropertyWithPrefix2
+{
+	@BoundProperty(name = ".value")
+	String value();
+}
+
+@BoundObject(sourcePath = "${substitution1}/path_part1/${substitution2}/path_part2/endPart")
+interface PropertyWithPrefix3
+{
+	@BoundProperty(name = ".configuration.${type}.memory.limit")
+	String value();
+}
+
+
+@BoundObject(sourcePath = "${substitution1}/path_part1/${substitution2}/path_part2/endPart")
+interface PropertyWithPrefix4
+{
+	@BoundProperty(name = "configuration.${type}.@{group}.limit.@{border}")
+	String value(String group, String border);
 }
