e5c76f0b5bdba574598100c75fde806e793b5e0f
Oleg Vyalyh <oleg.vyalyh@2me.link>
2019-01-24 00:40:47 +0200

    Added tests.

--- src/test/java/com/configlinker/tests/PropertyVariableSubstitutionTest.java
+++ src/test/java/com/configlinker/tests/PropertyVariableSubstitutionTest.java
@@ -1,23 +1,111 @@
 package com.configlinker.tests;
 
 
+import com.configlinker.FactorySettingsBuilder;
 import com.configlinker.annotations.BoundObject;
 import com.configlinker.annotations.BoundProperty;
+import com.configlinker.deserializers.DateType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.Date;
 import java.util.List;
 
 
 @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 class PropertyVariableSubstitutionTest extends AbstractBaseTest
 {
-	// propertyNamePrefix in BoundObject
+	@Test
+	void test_valueWithPrefix()
+	{
+		Prop_ValueWithPrefix properties = getSingleConfigInstance(Prop_ValueWithPrefix.class);
+		
+	}
 	
 	@Test
-	void test1()
+	void test_valueWithAndWithoutPrefix()
 	{
+		Prop_ValueWithAndWithoutPrefix properties = getSingleConfigInstance(Prop_ValueWithAndWithoutPrefix.class);
+		
+	}
 	
+	@Test
+	void test_sourcePathWithVar()
+	{
+		FactorySettingsBuilder builder = FactorySettingsBuilder.create().addParameter("subfolder", "config");
+		Prop_SourcePathWithVar properties = getSingleConfigInstance(builder, Prop_SourcePathWithVar.class);
+		
+	}
+	
+	@Test
+	void test_prefixWithVar()
+	{
+		FactorySettingsBuilder builder = FactorySettingsBuilder.create().addParameter("part", "languages");
+		Prop_PrefixWithVar properties = getSingleConfigInstance(builder, Prop_PrefixWithVar.class);
+		
+		
+	}
+	
+	@Test
+	void test_valueWithVar()
+	{
+		FactorySettingsBuilder builderC = FactorySettingsBuilder.create().addParameter("lang", "C");
+		Prop_ValueWithVar propertiesC = getSingleConfigInstance(builderC, Prop_ValueWithVar.class);
+		
+		FactorySettingsBuilder builderCpp = FactorySettingsBuilder.create().addParameter("lang", "Cpp");
+		Prop_ValueWithVar propertiesCpp = getSingleConfigInstance(builderCpp, Prop_ValueWithVar.class);
+		
+		FactorySettingsBuilder builderCsh = FactorySettingsBuilder.create().addParameter("lang", "Csh");
+		Prop_ValueWithVar propertiesCsh = getSingleConfigInstance(builderCsh, Prop_ValueWithVar.class);
+		
+	}
+	
+	@Test
+	void test_sourcePathWithVar_PrefixWithVar_ValuesWithVar_ValueWithAndWithoutPrefix()
+	{
+		FactorySettingsBuilder builder = FactorySettingsBuilder.create()
+			.addParameter("subfolder", "config")
+			.addParameter("part", "languages")
+			.addParameter("lang", "PHP");
+		Prop_VarInAllParts properties = getSingleConfigInstance(builder, Prop_VarInAllParts.class);
+		
+		
+	}
+	
+	@Test
+	void test_dynamicPropString_VarInAllParts()
+	{
+		FactorySettingsBuilder builder = FactorySettingsBuilder.create()
+			.addParameter("subfolder", "config")
+			.addParameter("globalPrefix", "programming")
+			.addParameter("lang", "PHP");
+		DynamicPropString_VarInAllParts properties = getSingleConfigInstance(builder, DynamicPropString_VarInAllParts.class);
+		
+		
+	}
+	
+	@Test
+	void test_dynamicPropStringEnum_VarInAllParts()
+	{
+		FactorySettingsBuilder builder = FactorySettingsBuilder.create()
+			.addParameter("subfolder", "config")
+			.addParameter("globalPrefix", "programming")
+			.addParameter("part", "languages");
+		DynamicPropStringEnum_VarInAllParts properties = getSingleConfigInstance(builder, DynamicPropStringEnum_VarInAllParts.class);
+		
+		
+	}
+	
+	@Test
+	void test_dynamicProp_VarInAllParts_TrackChanges()
+	{
+		FactorySettingsBuilder builder = FactorySettingsBuilder.create()
+			.addParameter("subfolder", "config")
+			.addParameter("globalPrefix", "programming")
+			.addParameter("part", "languages");
+		DynamicProp_VarInAllParts_TrackChanges properties = getSingleConfigInstance(builder, DynamicProp_VarInAllParts_TrackChanges.class);
+		
+		
 	}
 }
 
@@ -36,17 +124,17 @@ enum Lang
 interface Prop_ValueWithPrefix
 {
 	@BoundProperty(name = ".java.priority")
-	String value();
+	int javaPriority();
 }
 
 @BoundObject(sourcePath = "./configs/variable_substitution.properties", propertyNamePrefix = "programming.languages")
 interface Prop_ValueWithAndWithoutPrefix
 {
 	@BoundProperty(name = ".java.priority")
-	String value();
+	int cPriority();
 	
 	@BoundProperty(name = "programming.languages.java.designed")
-	String value2();
+	String cAuthor();
 }
 
 @BoundObject(sourcePath = "./${subfolder}/variable_substitution.properties")
@@ -59,25 +147,25 @@ interface Prop_SourcePathWithVar
 @BoundObject(sourcePath = "./configs/variable_substitution.properties", propertyNamePrefix = "programming.${part}")
 interface Prop_PrefixWithVar
 {
-	@BoundProperty(name = ".java.priority")
-	String value();
+	@BoundProperty(name = ".python.priority")
+	int pythonPriority();
 }
 
 @BoundObject(sourcePath = "./configs/variable_substitution.properties")
 interface Prop_ValueWithVar
 {
 	@BoundProperty(name = "programming.languages.${lang}.designed")
-	String value();
+	String langAuthor();
 }
 
 @BoundObject(sourcePath = "./${subfolder}/variable_substitution.properties", propertyNamePrefix = "programming.${part}")
-interface Prop_SourcePathWithVar_PrefixWithVar_ValuesWithVar_ValueWithAndWithoutPrefix
+interface Prop_VarInAllParts
 {
 	@BoundProperty(name = ".${lang}.priority")
-	String value();
+	int langPriority();
 	
-	@BoundProperty(name = "programming.languages.${lang}.designed")
-	String value2();
+	@BoundProperty(name = "programming.${part}.${lang}.birthday", customType = DateType.DateOnly.class)
+	Date langBirthday();
 }
 
 
@@ -94,6 +182,17 @@ interface DynamicPropString_VarInAllParts
 @BoundObject(sourcePath = "./${subfolder}/variable_substitution.properties", propertyNamePrefix = "${globalPrefix}")
 interface DynamicPropStringEnum_VarInAllParts
 {
-	@BoundProperty(name = ".${part}.@{langName}.@{info}")
-	String langInfo(String langName, Info info);
+	@BoundProperty(name = ".${part}.@{lang}.@{info}")
+	String langInfo(Lang lang, Info info);
+}
+
+
+@BoundObject(sourcePath = "./${subfolder}/variable_substitution.properties", propertyNamePrefix = "${globalPrefix}")
+interface DynamicProp_VarInAllParts_TrackChanges
+{
+	@BoundProperty(name = ".${part}.@{lang}.@{info}")
+	String langInfo(Lang lang, Info info);
+	
+	@BoundProperty(name = "programming.${part}.@{langName}.designed")
+	String langAuthor(String langName);
 }
