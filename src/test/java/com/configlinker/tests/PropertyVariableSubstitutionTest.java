5206a860a562f9a970cf582e15519386c3217294
Oleg Vyalyh <oleg.vyalyh@2me.link>
2019-01-23 20:41:22 +0200

    Added templates for PropertyVariableSubstitutionTest.

--- src/test/java/com/configlinker/tests/PropertyVariableSubstitutionTest.java
+++ src/test/java/com/configlinker/tests/PropertyVariableSubstitutionTest.java
@@ -6,6 +6,8 @@ import com.configlinker.annotations.BoundProperty;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.List;
+
 
 @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 class PropertyVariableSubstitutionTest extends AbstractBaseTest
@@ -20,31 +22,78 @@ class PropertyVariableSubstitutionTest extends AbstractBaseTest
 }
 
 
-@BoundObject(sourcePath = "path_part2/endPart", propertyNamePrefix = "servers.${type}.srv1.configuration")
-interface PropertyWithPrefix2
+enum Info
 {
-	@BoundProperty(name = ".value")
+	priority, designed, birthday;
+}
+
+enum Lang
+{
+	Java, Python, JavaScript, C, Cpp, Csh, PHP;
+}
+
+@BoundObject(sourcePath = "./configs/variable_substitution.properties", propertyNamePrefix = "programming.languages")
+interface Prop_ValueWithPrefix
+{
+	@BoundProperty(name = ".java.priority")
 	String value();
 }
 
-@BoundObject(sourcePath = "${substitution1}/path_part1/${substitution2}/path_part2/endPart")
-interface PropertyWithPrefix1
+@BoundObject(sourcePath = "./configs/variable_substitution.properties", propertyNamePrefix = "programming.languages")
+interface Prop_ValueWithAndWithoutPrefix
 {
-	@BoundProperty(name = "ordinary.value")
+	@BoundProperty(name = ".java.priority")
+	String value();
+	
+	@BoundProperty(name = "programming.languages.java.designed")
+	String value2();
+}
+
+@BoundObject(sourcePath = "./${subfolder}/variable_substitution.properties")
+interface Prop_SourcePathWithVar
+{
+	@BoundProperty(name = "programming.languages")
+	List<Lang> languages();
+}
+
+@BoundObject(sourcePath = "./configs/variable_substitution.properties", propertyNamePrefix = "programming.${part}")
+interface Prop_PrefixWithVar
+{
+	@BoundProperty(name = ".java.priority")
 	String value();
 }
 
-@BoundObject(sourcePath = "${substitution1}/path_part1/${substitution2}/path_part2/endPart")
-interface PropertyWithPrefix3
+@BoundObject(sourcePath = "./configs/variable_substitution.properties")
+interface Prop_ValueWithVar
 {
-	@BoundProperty(name = ".configuration.${type}.memory.limit")
+	@BoundProperty(name = "programming.languages.${lang}.designed")
 	String value();
 }
 
-
-@BoundObject(sourcePath = "${substitution1}/path_part1/${substitution2}/path_part2/endPart")
-interface PropertyWithPrefix4
+@BoundObject(sourcePath = "./${subfolder}/variable_substitution.properties", propertyNamePrefix = "programming.${part}")
+interface Prop_SourcePathWithVar_PrefixWithVar_ValuesWithVar_ValueWithAndWithoutPrefix
 {
-	@BoundProperty(name = "configuration.${type}.@{group}.limit.@{border}")
-	String value(String group, String border);
+	@BoundProperty(name = ".${lang}.priority")
+	String value();
+	
+	@BoundProperty(name = "programming.languages.${lang}.designed")
+	String value2();
+}
+
+
+@BoundObject(sourcePath = "./${subfolder}/variable_substitution.properties", propertyNamePrefix = "${globalPrefix}")
+interface DynamicPropString_VarInAllParts
+{
+	@BoundProperty(name = ".${part}.@{langName}.@{info}")
+	String langInfo(String langName, String info);
+	
+	@BoundProperty(name = "programming.${part}.@{langName}.designed")
+	String langAuthor(String langName);
+}
+
+@BoundObject(sourcePath = "./${subfolder}/variable_substitution.properties", propertyNamePrefix = "${globalPrefix}")
+interface DynamicPropStringEnum_VarInAllParts
+{
+	@BoundProperty(name = ".${part}.@{langName}.@{info}")
+	String langInfo(String langName, Info info);
 }
